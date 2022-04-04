1d12121ba6567a16b348c3e31b951597d036baf2
Jonathan Sivahera <jsivahera@icloud.com>
2022-04-04 19:27:13 +0200

    Style the header section

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,17 +4,16 @@ import '../css/Header.css';
 import logo from '../assets/icons/planet.png';
 
 const Header = () => (
-  <header className="w-full flex justify-between items-center p-4">
-    <NavLink to="/">
-      <img className="w-1/2" src={logo} alt="planet-logo" />
-      {' '}
-      <span>Space Traveler&#39;s Hub</span>
+  <header className="w-11/12 flex justify-between items-center px-2 py-4 Header">
+    <NavLink className="flex justify-between items-center" to="/">
+      <img className="w-12 h-auto" src={logo} alt="planet-logo" />
+      <span className="ml-4 text-2xl">Space Traveler&#39;s Hub</span>
     </NavLink>
 
-    <nav>
-      <NavLink to="/rockets">Rockets</NavLink>
-      <NavLink to="/missions">Missions</NavLink>
-      <NavLink to="/profile">My Profile</NavLink>
+    <nav className="flex items-center justify-between w-1/5">
+      <NavLink className="pr-2 text-blue-600 NavLink" to="/rockets">Rockets</NavLink>
+      <NavLink className="px-2 text-blue-600 NavLink" to="/missions">Missions</NavLink>
+      <NavLink className="pl-2 text-blue-600 NavLink" to="/profile">My Profile</NavLink>
     </nav>
   </header>
 );
