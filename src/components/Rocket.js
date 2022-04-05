c42fde38ed0593b4b269ebdaf2cf01c8b24a1177
Jonathan Sivahera <jsivahera@icloud.com>
2022-04-05 22:29:44 +0200

    Remove reserved prop

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -4,8 +4,13 @@ import '../css/Rocket.css';
 
 const Rocket = ({ rocket }) => {
   const {
-    description, image, name, reserved,
+    description, image, name,
   } = rocket;
+
+  const isReserved = (rocket) => {
+    if (rocket.reserved) return true;
+    return false;
+  };
 
   return (
     <div className="flex items-center w-full gap-4 rocket-container">
@@ -14,7 +19,7 @@ const Rocket = ({ rocket }) => {
         <h2 className="text-xl font-semibold">{name}</h2>
         <p className="text-md">{description}</p>
         <button className="text-sm text-white bg-blue-500 rounded-md p-2" type="button">
-          {reserved ? 'Cancel Reservation' : 'Reserve Rocket'}
+          {isReserved(rocket) ? 'Cancel Reservation' : 'Reserve Rocket'}
         </button>
       </div>
     </div>
--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -24,7 +24,6 @@ const getRockets = () => async (dispatch) => {
           name: rocket.rocket_name,
           description: rocket.description,
           image: rocket.flickr_images[0],
-          reserved: false,
         });
       });
 
