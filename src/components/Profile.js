74b1d413a762bedf676a6e26a94f95c03454df86
amira <amiraabouhadid@gmail.com>
2022-04-06 13:12:03 +0200

    add joined missions to profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,4 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Profile = () => (<div>Profile</div>);
+const Profile = (props) => {
+  const { missions } = props;
+  return (
+    <div className="p-5 row">
+      <div className="col-6">
+        <h2>
+          My Missions
+        </h2>
+        <div>
+          {
+        missions.filter((mission) => mission.reserved).map((mission) => (
+          <p className="border p-3" key={mission.id}>{mission.name}</p>
+        ))
+        }
+        </div>
+      </div>
+    </div>
+  );
+};
+Profile.propTypes = {
+  missions: PropTypes.instanceOf(Array).isRequired,
+};
 export default Profile;
