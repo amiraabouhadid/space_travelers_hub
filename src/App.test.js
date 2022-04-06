0fd926802cd255014e8af7db5555a9a07c76c97e
amira <amiraabouhadid@gmail.com>
2022-04-06 16:05:11 +0200

    add unit tests for missions component and missions in profile component rendering

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,79 @@
 import { render, screen } from '@testing-library/react';
-
+import { Provider } from 'react-redux';
+import Store from './redux/configureStore';
 import Missions from './components/Missions';
+import Profile from './components/Profile';
+
+describe('Profile page component', () => {
+  test('renders profile headers', () => {
+    const missions = [{ id: '123', name: 'Falcon', description: 'a dummy text created for testing purposes' }];
+    const rockets = [{
+      id: '1329', name: 'myStar', description: 'second dummy text', image: 'cool pic',
+    }];
+    render(
+      <Provider store={Store}>
+        <Profile missions={missions} rockets={rockets} />
+      </Provider>,
+    );
+    const profilePageHeaders = screen.getAllByText(/\b(my missions)\b|\b(my rockets)\b/i);
+    expect(profilePageHeaders).toHaveLength(2);
+  });
+});
 
 describe('Missions page component', () => {
-  test('renders missions fetched ', () => {
-    const missions = [{ id: '123', name: 'testMission', description: 'a dummy mission created for testing purposes' }];
-    render(<Missions missions={missions} />);
-    const tableHeader = screen.getByText(/mission/i);
-    expect(tableHeader).toBeInTheDocument();
+  test('renders  missions table header ', () => {
+    const missions = [{ id: '123', name: 'Falcon', description: 'a dummy text created for testing purposes' }];
+
+    render(
+      <Provider store={Store}>
+        <Missions missions={missions} />
+      </Provider>,
+    );
+    const missionsTableHeader = screen.getAllByText(/\b(^mission)\b|\b(description)\b|\b(status)\b/i);
+    expect(missionsTableHeader).toHaveLength(3);
+  });
+  test('renders  mission  name', () => {
+    const missions = [{ id: '123', name: 'Falcon', description: 'a dummy text created for testing purposes' }];
+
+    render(
+      <Provider store={Store}>
+        <Missions missions={missions} />
+      </Provider>,
+    );
+    const missionName = screen.getAllByText(/\bfalcon\b/i);
+    expect(missionName).toHaveLength(1);
+  });
+  test('renders  mission  description ', () => {
+    const missions = [{ id: '123', name: 'Falcon', description: 'a dummy text created for testing purposes' }];
+
+    render(
+      <Provider store={Store}>
+        <Missions missions={missions} />
+      </Provider>,
+    );
+    const missionDescription = screen.getAllByText(/\ba dummy text created for testing purposes\b/i);
+    expect(missionDescription).toHaveLength(1);
+  });
+  test('renders  mission  status badge before joining mission', () => {
+    const missions = [{ id: '123', name: 'Falcon', description: 'a dummy text created for testing purposes' }];
+
+    render(
+      <Provider store={Store}>
+        <Missions missions={missions} />
+      </Provider>,
+    );
+    const missionDescription = screen.getAllByText(/\bnot a member\b/i);
+    expect(missionDescription).toHaveLength(1);
+  });
+  test('renders mission join mission button before joining mission', () => {
+    const missions = [{ id: '123', name: 'Falcon', description: 'a dummy text created for testing purposes' }];
+
+    render(
+      <Provider store={Store}>
+        <Missions missions={missions} />
+      </Provider>,
+    );
+    const missionDescription = screen.getAllByText(/\bjoin mission\b/i);
+    expect(missionDescription).toHaveLength(1);
   });
 });
