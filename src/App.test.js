6132be088818ee2d78d4650352bd5f3387f1141b
amira <amiraabouhadid@gmail.com>
2022-04-06 16:36:55 +0200

    add tests for join mission button functionality i.e change status badge, join button in missions page and add joined missions to profile page

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
 import Store from './redux/configureStore';
 import Missions from './components/Missions';
 import Profile from './components/Profile';
-
 import App from './App';
 
 describe('Header component', () => {
@@ -50,6 +50,18 @@ describe('Header component', () => {
   });
 });
 describe('Profile page component', () => {
+  test('renders correctly', () => {
+    const missions = [{ id: '123', name: 'Falcon', description: 'a dummy text created for testing purposes' }];
+    const rockets = [{
+      id: '1329', name: 'myStar', description: 'second dummy text', image: 'cool pic',
+    }];
+    const profilePage = renderer.create(
+      <Provider store={Store}>
+        <Profile missions={missions} rockets={rockets} />
+      </Provider>,
+    ).toJSON();
+    expect(profilePage).toMatchSnapshot();
+  });
   test('renders profile headers', () => {
     const missions = [{ id: '123', name: 'Falcon', description: 'a dummy text created for testing purposes' }];
     const rockets = [{
@@ -63,9 +75,38 @@ describe('Profile page component', () => {
     const profilePageHeaders = screen.getAllByText(/\b(my missions)\b|\b(my rockets)\b/i);
     expect(profilePageHeaders).toHaveLength(2);
   });
+  test('adds joined missions to profile when join mission button is clicked ', () => {
+    const missions = [{ id: '123', name: 'Falcon', description: 'a dummy text created for testing purposes' }];
+    const rockets = [{
+      id: '1329', name: 'myStar', description: 'second dummy text', image: 'cool pic',
+    }];
+    render(
+      <Provider store={Store}>
+        <Missions missions={missions} />
+      </Provider>,
+    );
+    const missionJoinButton = screen.getByText(/\bjoin mission\b/i);
+    userEvent.click(missionJoinButton);
+    const profilePageAfterJoinMission = renderer.create(
+      <Provider store={Store}>
+        <Profile missions={missions} rockets={rockets} />
+      </Provider>,
+    ).toJSON();
+    expect(profilePageAfterJoinMission).toMatchSnapshot();
+  });
 });
 
 describe('Missions page component', () => {
+  test('renders correctly', () => {
+    const missions = [{ id: '123', name: 'Falcon', description: 'a dummy text created for testing purposes' }];
+
+    const missionsPage = renderer.create(
+      <Provider store={Store}>
+        <Missions missions={missions} />
+      </Provider>,
+    ).toJSON();
+    expect(missionsPage).toMatchSnapshot();
+  });
   test('renders  missions table header ', () => {
     const missions = [{ id: '123', name: 'Falcon', description: 'a dummy text created for testing purposes' }];
 
@@ -107,10 +148,10 @@ describe('Missions page component', () => {
         <Missions missions={missions} />
       </Provider>,
     );
-    const missionDescription = screen.getAllByText(/\bnot a member\b/i);
-    expect(missionDescription).toHaveLength(1);
+    const missionBadge = screen.getAllByText(/\bnot a member\b/i);
+    expect(missionBadge).toHaveLength(1);
   });
-  test('renders mission join mission button before joining mission', () => {
+  test('renders  join mission button before joining mission', () => {
     const missions = [{ id: '123', name: 'Falcon', description: 'a dummy text created for testing purposes' }];
 
     render(
@@ -118,7 +159,25 @@ describe('Missions page component', () => {
         <Missions missions={missions} />
       </Provider>,
     );
-    const missionDescription = screen.getAllByText(/\bjoin mission\b/i);
-    expect(missionDescription).toHaveLength(1);
+    const missionButton = screen.getAllByText(/\bjoin mission\b/i);
+    expect(missionButton).toHaveLength(1);
+  });
+
+  test('changes status badge and join button when mission is joined ', () => {
+    const missions = [{ id: '123', name: 'Falcon', description: 'a dummy text created for testing purposes' }];
+
+    render(
+      <Provider store={Store}>
+        <Missions missions={missions} />
+      </Provider>,
+    );
+    const joinButton = screen.getByText(/\bjoin mission\b/i);
+    userEvent.click(joinButton);
+    const missionsPageAfterJoinMission = renderer.create(
+      <Provider store={Store}>
+        <Missions missions={missions} />
+      </Provider>,
+    ).toJSON();
+    expect(missionsPageAfterJoinMission).toMatchSnapshot();
   });
 });
